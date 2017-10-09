5136e3216ef4a23dee47b8408053e5a35b415ca1
dc1 <dc1@officepc8.home>
2017-10-09 17:07:47 +0100

    1message by dc

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -16,6 +16,8 @@ and open the template in the editor.
         // hello2
         
         // hello 3 this is the branch
+        // this mod is added to my_test_branch2
+        // this is also added to mb2
         ?>
     </body>
 </html>
